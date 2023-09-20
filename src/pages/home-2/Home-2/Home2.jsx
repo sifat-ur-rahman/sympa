7b24fc4d07e19a4234ad1d60164df21f4aeed6b9
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-20 17:35:29 +0600

    add

--- src/pages/home-2/Home-2/Home2.jsx
+++ src/pages/home-2/Home-2/Home2.jsx
@@ -1,5 +1,6 @@
 import Banner from "../home2Components/banner/Banner"
 import Home2AboutUs from "../home2Components/home2AboutUs/Home2AboutUs"
+import Home2CTA from "../home2Components/home2CTA/Home2CTA"
 import Home2Offer from "../home2Components/home2Offer/Home2Offer"
 
 
@@ -9,6 +10,7 @@ function Home2() {
       <Banner/>
       <Home2AboutUs/>
       <Home2Offer/>
+      <Home2CTA/>
     </div>
   )
 }
---
+++ src/pages/home-2/home2Components/home2CTA/Home2CTA.jsx
@@ -0,0 +1,24 @@
+import bgCTA from '../../../../assets/image/home2/bgCTA.png'
+
+
+function Home2CTA() {
+  return (
+    <div className="bg-orange-500 relative">
+        <img className='absolute top-0 left-0 w-14' src={bgCTA} alt="" />
+        <section className="px-16 py-4 flex justify-around">
+            <div>
+                <h3 className="text-white text-4xl font-semibold  capitalize leading-[50px]">Streamlining Movement: Uniting the Globe, <br /> One Delivery at a Time!</h3>
+
+            </div>
+            <div className="flex items-center justify-between">
+                
+            <button className='btn-purple-white-border common-btn-purple'>Contact Us</button>
+            <button className='btn-white-border common-btn ml-6'>Learn more</button>
+
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Home2CTA
