c58045741082f2cc0639f19de238d703f9e118d4
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-21 17:20:44 +0600

    add

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -2,7 +2,7 @@ import CountUp from "react-countup";
 
 function Counter({ number,title}) {
   return (
-    <div className="text-4xl">
+    <div className="text-5xl">
       <CountUp enableScrollSpy duration={5} className="counter" end={number} />
       <span>{title}</span>
     </div>
--- src/pages/home/homeComponents/homePricing/HomePricing.jsx
+++ src/pages/home/homeComponents/homePricing/HomePricing.jsx
@@ -33,54 +33,54 @@ function HomePricing() {
                   </div>
                 </div>
                 <hr />
-                <div className="mt-6">
+                <div className="mt-10">
                 <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Unlimited Updates and products</p>
                 <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom permission</p>
                 <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom design and features</p>
                 <p className="text-zinc-500 text-base font-normal font-['Rubik'] leading-[30px] mb-2 flex items-center line-through"><BsPatchCheck className="text-zinc-500 mr-2 "/> Unlimited Updates</p>
                 <p className=" text-zinc-500 text-base font-normal font-['Rubik'] leading-[30px] mb-12 flex items-center line-through"><BsPatchCheck className="text-zinc-500 mr-2"/> Custom Interface</p>
                 </div>
-                <button className="btn common-btn justify-items-center  mb-4">Choose Plan<BsArrowUpRight className="ms-2"/> </button>
+                <button className="btn common-btn justify-items-center  ">Choose Plan<BsArrowUpRight className="ms-2"/> </button>
             </div>
-            <div data-aos="fade-down" className='shadow shadow-slate-200 hover:border-2 hover:border-orange-300 p-4 mr-5 cursor-default bg-white'>
+            <div data-aos="fade-down" className='shadow shadow-slate-200 hover:border-2 hover:border-orange-300 p-10 mr-5 cursor-default bg-white'>
                 <div className="flex items-center justify-between mb-8 mt-4">
                   <div >
-                    <h4 className='text-xl font-bold'>Professional</h4>
-                    <p className="text-zinc-500 text-base font-normal">Recommended</p>
+                    <h4 className='text-2xl font-semibold  leading-loose'>Professional</h4>
+                    <p className="text-zinc-500 text-base font-normal font-['Rubik'] leading-loose">Recommended</p>
                   </div>
-                  <div className="bg-slate-50">
-                    <h2 className="text-2xl font-bold">$5.50</h2>
-                    <p className="text-zinc-500 text-base font-normal">Per Month</p>
+                  <div className="bg-slate-50 p-2.5">
+                    <h2 className="text-[32px] font-semibold">$5.50</h2>
+                    <p className="w-[77px] h-5 text-zinc-500 text-base font-normal font-['Rubik'] leading-[30px]">Per Month</p>
                   </div>
                 </div>
                 <hr />
                 <div className="mt-6">
-                <p className=" text-base font-normal mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Unlimited Updates and products</p>
-                <p className=" text-base font-normal mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom permission</p>
-                <p className=" text-base font-normal mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom design and features</p>
-                <p className="text-base font-normal mb-2 flex items-center "><BsPatchCheck className="text-orange-600 mr-2 "/> Unlimited Updates</p>
-                <p className=" text-zinc-500 text-base font-normal mb-12 flex items-center line-through"><BsPatchCheck className="text-zinc-500 mr-2"/> Custom Interface</p>
+                <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Unlimited Updates and products</p>
+                <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom permission</p>
+                <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom design and features</p>
+                <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center "><BsPatchCheck className="text-orange-600 mr-2 "/> Unlimited Updates</p>
+                <p className=" text-zinc-500 text-base font-normal font-['Rubik'] leading-[30px] mb-12 flex items-center line-through"><BsPatchCheck className="text-zinc-500 mr-2"/> Custom Interface</p>
                 </div>
                 <button className="btn common-btn justify-items-center  mb-4">Choose Plan<BsArrowUpRight className="ms-2"/> </button>
             </div>
-            <div data-aos="fade-left" className='shadow shadow-slate-200 hover:border-2 hover:border-orange-300 p-4 mr-5 cursor-default bg-white'>
+            <div data-aos="fade-left" className='shadow shadow-slate-200 hover:border-2 hover:border-orange-300 p-10 mr-5 cursor-default bg-white'>
                 <div className="flex items-center justify-between mb-8 mt-4">
                   <div >
-                    <h4 className='text-xl font-bold'>Ultimate</h4>
-                    <p className="text-zinc-500 text-base font-normal">Best Value</p>
+                    <h4 className='text-2xl font-semibold  leading-loose'>Ultimate</h4>
+                    <p className="text-zinc-500 text-base font-normal font-['Rubik'] leading-loose">Best Value</p>
                   </div>
-                  <div className="bg-slate-50">
-                    <h2 className="text-2xl font-bold">$7.50</h2>
-                    <p className="text-zinc-500 text-base font-normal">Per Month</p>
+                  <div className="bg-slate-50 p-2.5">
+                    <h2 className="text-[32px] font-semibold">$7.50</h2>
+                    <p className="w-[77px] h-5 text-zinc-500 text-base font-normal font-['Rubik'] leading-[30px]">Per Month</p>
                   </div>
                 </div>
                 <hr />
                 <div className="mt-6">
-                <p className=" text-base font-normal mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Unlimited Updates and products</p>
-                <p className=" text-base font-normal mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom permission</p>
-                <p className=" text-base font-normal mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom design and features</p>
-                <p className=" text-base font-normal mb-2 flex items-center "><BsPatchCheck className="text-orange-600 mr-2 "/> Unlimited Updates</p>
-                <p className="  text-base font-normal mb-12 flex items-center "><BsPatchCheck className="text-orange-600 mr-2"/> Custom Interface</p>
+                <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Unlimited Updates and products</p>
+                <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom permission</p>
+                <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center"><BsPatchCheck className="text-orange-600 mr-2"/> Custom design and features</p>
+                <p className=" text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center "><BsPatchCheck className="text-orange-600 mr-2 "/> Unlimited Updates</p>
+                <p className="  text-base font-normal font-['Rubik'] leading-loose mb-2 flex items-center "><BsPatchCheck className="text-orange-600 mr-2"/> Custom Interface</p>
                 </div>
                 <button className="btn common-btn justify-items-center  mb-4">Choose Plan<BsArrowUpRight className="ms-2"/> </button>
             </div>
