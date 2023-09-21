505c02e98bf49ffcf51973cb00c4459c89b299fd
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-21 17:54:14 +0600

    add

--- src/pages/home/homeComponents/homeBlog/HomeBlog.jsx
+++ src/pages/home/homeComponents/homeBlog/HomeBlog.jsx
@@ -12,7 +12,7 @@ function HomeBlog() {
     <div className="bg-slate-50  relative">
         <img loading="lazy" className="absolute left-10 -top-10 w-44 " src={bgImage} alt="" />
         <div className="pt-10"> 
-        <section data-aos="fade-down"  className=" mx-80 flex items-center justify-between py-14">
+        <section data-aos="fade-down"  className=" mx-52 flex items-center justify-between py-14">
             <div>
             <p className='text-base font-normal text-indigo-600 flex items-center mb-3'><FaDotCircle className='font-sans mr-1'/> OUR BLOG</p>
             <h3 className="sectionTitle">Checkout Our Latest  <br /> News & Blog.</h3>
@@ -23,47 +23,47 @@ function HomeBlog() {
 
             </div>
         </section>
-        <section className="grid grid-cols-3 gap-6 items-center mx-80   pb-20">
+        <section className="grid grid-cols-3 gap-6 items-center mx-52   pb-20">
             <div data-aos="fade-up-right" className="bg-white relative border  hover:border-orange-400">
-                <img src={blog01} alt="" />
-                <p className="absolute left-2 top-56 bg-orange-500 text-white p-3 rounded-lg font-semibold">30 May 2023</p>
-                <div className="flex items-center justify-start mt-2 px-4">
+                <img className="w-full" src={blog01} alt="" />
+                <p className="absolute left-2 top-72 bg-orange-500 text-white p-3 rounded-lg font-semibold">30 May 2023</p>
+                <div className="flex items-center justify-start mt-4 px-4">
                     <p className="flex items-center mr-8"><FaUserAlt className="text-orange-400 mr-1"/> By admin</p>
                     <p className="flex items-center"><FaComments className="text-orange-400 mr-1"/>(05) </p>
                 </div>
-                <div className="my-4 px-4">
-                    <h4 className="text-2xl mb-2">Business Solutions that move you forward growth of your business.</h4>
-                    <p className="text-zinc-500 text-base font-normal">It is a long established fact that a reader williljl be distracted by the readable content of amjlk page when looking at its layout.</p>
+                <div className="my-8 px-4">
+                    <h4 className="text-2xl font-semibold leading-[30px] mb-2">Business Solutions that move you forward growth of your business.</h4>
+                    <p className="text-zinc-500 text-base font-normal mt-3">It is a long established fact that a reader williljl be distracted by the readable content of amjlk page when looking at its layout.</p>
                 </div>
                 <div className="ml-4 mb-4">
                 <button className="btn-purple common-btn-purple">Free consultion  <BsArrowUpRight className="ms-2"/> </button>
                 </div>
             </div>
             <div data-aos="fade-up" className=" bg-white relative border  hover:border-orange-400">
-                <img src={blog02} alt="" />
-                <p className="absolute left-2 top-48 bg-orange-500 text-white p-3 rounded-lg font-semibold">20 May 2023</p>
-                <div className="flex items-center justify-start mt-2 px-4">
+                <img className="w-full" src={blog02} alt="" />
+                <p className="absolute left-2 top-72 bg-orange-500 text-white p-3 rounded-lg font-semibold">20 May 2023</p>
+                <div className="flex items-center justify-start mt-4 px-4">
                     <p className="flex items-center mr-8"><FaUserAlt className="text-orange-400 mr-1"/> By admin</p>
                     <p className="flex items-center"><FaComments className="text-orange-400 mr-1"/>(09) </p>
                 </div>
-                <div className="my-4 px-4">
-                    <h4 className="text-2xl mb-2">Develop a strategy to foster the Grow of your enterprise.</h4>
-                    <p className="text-zinc-500 text-base font-normal">It is a long established fact that a reader williljl be distracted by the readable content of amjlk page when looking at its layout.</p>
+                <div className="my-8 px-4">
+                    <h4 className="text-2xl font-semibold leading-[30px]mb-2">Develop a strategy to foster the Grow of your enterprise.</h4>
+                    <p className="text-zinc-500 text-base font-normal mt-3">It is a long established fact that a reader williljl be distracted by the readable content of amjlk page when looking at its layout.</p>
                 </div>
                 <div className="ml-4 mb-4">
                 <button className="btn-purple common-btn-purple">Free consultion  <BsArrowUpRight className="ms-2"/> </button>
                 </div>
             </div>
             <div data-aos="fade-up-left" className=" bg-white relative border  hover:border-orange-400">
-                <img src={blog03} alt="" />
-                <p className="absolute left-2 top-48 bg-orange-500 text-white p-3 rounded-lg font-semibold">10 May 2023</p>
-                <div className="flex items-center justify-start mt-2 px-4">
+                <img className="w-full" src={blog03} alt="" />
+                <p className="absolute left-2 top-72 bg-orange-500 text-white p-3 rounded-lg font-semibold">10 May 2023</p>
+                <div className="flex items-center justify-start mt-4 px-4">
                     <p className="flex items-center mr-8"><FaUserAlt className="text-orange-400 mr-1"/> By admin</p>
                     <p className="flex items-center"><FaComments className="text-orange-400 mr-1"/>(07) </p>
                 </div>
-                <div className="my-4 px-4">
-                    <h4 className="text-2xl mb-2">Five effective way to enhance the growth of your business.</h4>
-                    <p className="text-zinc-500 text-base font-normal">It is a long established fact that a reader williljl be distracted by the readable content of amjlk page when looking at its layout.</p>
+                <div className="my-8 px-4">
+                    <h4 className="text-2xl font-semibold leading-[30px] mb-2">Five effective way to enhance the growth of your business.</h4>
+                    <p className="text-zinc-500 text-base font-normal mt-3">It is a long established fact that a reader williljl be distracted by the readable content of amjlk page when looking at its layout.</p>
                 </div>
                 <div className="ml-4 mb-4">
                 <button className="btn-purple common-btn-purple">Free consultion  <BsArrowUpRight className="ms-2"/> </button>
